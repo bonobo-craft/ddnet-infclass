36cb3a8e4744e385f2ee2a9550ba76ac0470a833
Pure luck <pure_luck@fastmail.com>
2020-10-04 23:57:22 +0300

    upload automatically

--- poeditor/renew.sh
+++ poeditor/renew.sh
@@ -3,4 +3,5 @@
 ./get_all_languages.sh || exit 1
 ruby from_poeditor.rb || exit 1
 cp translations.json_new ../build/translations.json && echo OK
+cp translations.json 'infection@teeworlds-ams:/home/infection/teeworlds-infection-ddnet'  
 
